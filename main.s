ffd94f621f956aa346f56c622e275be4c1e7c9e1
Dark-GreenCat <tranquanghuy3008@outlook.com>
2024-05-12 22:10:13 +0700

    [ADD] Add LEDMATRIX_DisplayPixel()

--- main.s
+++ main.s
@@ -8,6 +8,15 @@
 
 # Define ecall IDs
 .equ    EXIT, 10
+.equ    LEDMATRIX_SET_PIXEL, 0x100
+
+# #######################################
+# #####      CONSTANT VARIABLE     ######
+# #######################################
+.data
+
+# Define color
+COLOR_DEFAULT: .word 0xFFFF00
 
 
 # #######################################
@@ -18,8 +27,30 @@
 # Start of code (entry point). Must be put in the beginning
 _start:
 
+    # Set pixel at (2, 4) with default color
+    li a0, 2
+    li a1, 4
+    la t0, COLOR_DEFAULT
+    lw a2, 0(t0)
+    call LEDMATRIX_DisplayPixel
+
     # Exit program
     li a0, EXIT
     ecall
+
+# void LEDMATRIX_DisplayPixel(uint32_t x, uint32_t y, uint32_t color)
+.globl LEDMATRIX_DisplayPixel
+LEDMATRIX_DisplayPixel:
+    # Prepare data
+    slli a0, a0, 16         # x = x << 16
+    or   a1, a0, a1         # y = x | y
+
+    # Display pixel to Led Matrix
+    li a0, LEDMATRIX_SET_PIXEL
+    ecall
+
+    # Return from function
+    jr ra
+
 
 # End of program, leave a blank line afterwards is preferred
